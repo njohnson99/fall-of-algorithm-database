73596707520a6d8a2f653c3852614c3f2516af78
njohnson99 <narij@andrew.cmu.edu>
2024-04-21 12:05:01 -0400

    wip placeholder

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script> 
+</script>
+
+<main>
+	<div id = "all-html">
+		<div id = "login">
+		This database is currently under development, to be released in June 2024.<br><br>
+
+		If you have any questions or would like to access our data, you can reach us at: narij [at] andrew.cmu.edu.
+	</div>
+	</div>
+
+
+</main>
+
+<style>
+	#login {
+		margin-top: 2cm;
+		margin-left: 2cm;
+		margin-right: 8cm;
+		margin-bottom: 2cm;
+		font-size: 20px;
+	}
+	#all-html {
+		margin-top: 2cm;
+		margin-left: 2cm;
+		margin-right: 2cm;
+		margin-bottom: 2cm;
+		font-size: 20px;
+		position: relative;
+	}
+</style>
---
+++ src/tutorials.js
@@ -0,0 +1,4 @@
+export { default as QA } from './QA.svelte';
+export { default as ObjectOverview } from './ObjectOverview.svelte';
+
+export { default as TutorialApp } from './tutorial/TutorialApp.svelte';
