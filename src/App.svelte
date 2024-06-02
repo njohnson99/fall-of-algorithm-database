fd9ead97d9a0dc4e18b149faa7cd507783769faf
njohnson99 <narij@andrew.cmu.edu>
2024-06-02 18:31:14 -0300

    src

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,23 @@
 <main>
 	<div id = "all-html">
 		<div id = "login">
-		This database is currently under development, to be released by June 2024.<br><br>
+		<h1>The Fall of an Algorithm: Case Dataset</h1>
+		As a supplement to our <a href = "https://arxiv.org/abs/2404.13802">research paper</a>, we release our dataset of coded information from 40 different real-world algorithmic systems to serve as a resource to aid future research and advocacy efforts. <br><br>
+		
+		Our dataset can be accessed and downloaded on <a href = "https://airtable.com/app7XjHALMukM66xq/shrJl1mW7V0dp8gxa">Airtable (link)</a>.<br><br>
 
-		If you have any questions or would like to access our data, you can reach us at: narij [at] andrew.cmu.edu.
-	</div>
+		⚠️ <b>This dataset was created in December 2023, using available public materials. Information in this dataset, such as whether an algorithmic system is abandoned or in-use, was last updated in 12/2023, and might now be out-of-date.</b> To learn more about a case, we recommend that you supplement the information and references provided in the spreadsheet with more up-to-date sources.<br><br>
+
+		<b>Request a correction:</b> While our team will <i>not</i> update the dataset to contain new information or events that occurred after December 2023, we <i>are</i> committed to ensuring that the dataset contains information that is correct, and is not misleading. If you spot any incorrect or misleading information in our dataset, please email us and we'll address it as soon as we can. <br>
+		<h2>Datasheet</h2>
+
+		We will create and link a <a href = "https://arxiv.org/abs/1803.09010">datasheet</a> for this dataset (WIP).
+		<h2>Citation</h2>
+		If you use the database, please <a href = "https://arxiv.org/abs/2404.13802">cite our paper</a>.
+
+		<h2>Contact</h2>
+		If you have any questions about the dataset or our work, please contact: narij at cmu dot andrew dot edu.
+		</div>
 	</div>
 
 
